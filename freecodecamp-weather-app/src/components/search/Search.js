f6288b404fe9b0c1c8face91a2681de48639be19
Abranka <davidabranka@gmail.com>
2022-11-30 16:35:55 +0100

    deployed

--- freecodecamp-weather-app/src/components/search/Search.js
+++ freecodecamp-weather-app/src/components/search/Search.js
@@ -1,86 +1,91 @@
-
 /*__________________________________________________I M P O R T S___________________________________________________________________*/
 
-import React, { useState } from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
-import { GeoApiUrl, geoApiOptions } from '../api'
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GeoApiUrl, geoApiOptions } from "../api";
+import PropTypes from "prop-types";
 
 /*________________________________________________M A I N  F U N C T I O N_____________________________________________________________________*/
 
 /*__↓__MAIN__FUNCTION__SEARCH__↓__ */
-const Search = ({onSearchChange}) => { // onSearchChange = le moment ou la valeur de notre rech' change
+const Search = ({ onSearchChange }) => {
+  // onSearchChange = le moment ou la valeur de notre rech' change
 
-/*________________________________________________V A R I A B L E S / C O N S T S_____________________________________________________________*/
+  /*________________________________________________V A R I A B L E S / C O N S T S_____________________________________________________________*/
 
   /*__↓__useState__↓__ */
-const [search, setSearch] = useState([]); // usestate() = (etat initial de l'app)
- // search = la valeur de AsyncPaginate * setSearch = la valeur au changement
+  const [search, setSearch] = useState([]); // usestate() = (etat initial de l'app)
+  // search = la valeur de AsyncPaginate * setSearch = la valeur au changement
 
-/*_____________INSIDE__FUNCTIONS(2)______________________*/
- 
- /*__↓__FUNCTION__loadOptions__↓__ */
-const loadOptions = (inputValue) => { //inputValue = la valeur tapé dans la barre qui servira de requete au données voulu
+  /*_____________INSIDE__FUNCTIONS(2)______________________*/
 
-    return fetch(`${GeoApiUrl}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+  /*__↓__FUNCTION__loadOptions__↓__ */
+  const loadOptions = (inputValue) => {
+    //inputValue = la valeur tapé dans la barre qui servira de requete au données voulu
 
-    .then(response => response.json())
-    .then(response => {
-      // function pour avoir les suggestions
-      return {
-        options: response.data.map((city) =>{ //? response.data.map
-          return {
-            value: `${city.latitude} ${city.longitude}`,
-            label: `${city.name} ${city.countryCode}`,
-          }
-        })
-      }
-    })
-    .catch(err => console.error(err));
-}
+    return fetch(
+      `${GeoApiUrl}/cities?minPopulation=10000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        // function pour avoir les suggestions
+        return {
+          options: response.data.map((city) => {
+            //? response.data.map
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name} ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
-/*__↓__FUNCTION__handleOnChange__↓__ */
-const handleOnChange = (searchData) => { // handleOnChange function qui contiendra  en parametre "searchData" qui récupère les données qu'on rentre dans "AsyncPaginate" la barre de recherche.
-    setSearch(searchData) // setSearch = updating our search
-    onSearchChange(searchData)
-};
+  /*__↓__FUNCTION__handleOnChange__↓__ */
+  const handleOnChange = (searchData) => {
+    // handleOnChange function qui contiendra  en parametre "searchData" qui récupère les données qu'on rentre dans "AsyncPaginate" la barre de recherche.
+    setSearch(searchData); // setSearch = updating our search
+    onSearchChange(searchData);
+  };
 
-/*______________________________________________________M A I N  F U N C T I O N  R E T U R N______________________________________________________________________*/
+  /*______________________________________________________M A I N  F U N C T I O N  R E T U R N______________________________________________________________________*/
 
-/*__↓__R E T U R N__↓__ */
+  /*__↓__R E T U R N__↓__ */
 
-// return = ce que l'app va affiché 
+  // return = ce que l'app va affiché
   return (
     <AsyncPaginate // ? What is AsyncPaginate ?
-    placeholder="Search for city..."
-    debounceTimeout={1000} // Délai de 6 second avant de request a nouveau
-    value={search} // la valeur contiendra la const search *
-    onChange={handleOnChange}
-     // onChange = Au moment ou la valeur change > la mettre a jour
-     // handleOnChange = prend en charge le moment en question
-     loadOptions={loadOptions} // propriété avec valeur "loadOptions" = 
-     /> 
-  )
-}
+      placeholder="Search for city..."
+      debounceTimeout={1000} // Délai de 6 second avant de request a nouveau
+      value={search} // la valeur contiendra la const search *
+      onChange={handleOnChange}
+      // onChange = Au moment ou la valeur change > la mettre a jour
+      // handleOnChange = prend en charge le moment en question
+      loadOptions={loadOptions} // propriété avec valeur "loadOptions" =
+    />
+  );
+};
 
 Search.propTypes = {
-  onSearchChange: PropTypes.func
-}
+  onSearchChange: PropTypes.func,
+};
 
 /*______________________________________________________E X P O R T S_______________________________________________________________*/
 
 /*__↓__EXPORT__↓__ */
 
-export default Search
+export default Search;
 
 /*_______________________________________________________S T E P S______________________________________________________________*/
 
 // On crée une function nommée Search
 // Dans cette function on y retourne la barre de recherche qui est "AsyncPaginate"
 // Dans AsyncPaginate on y met
-  // un placeholder = "la valeur du place holder"
-  // un debounceTimout = {delai entre chaque requete}
-  // value = {la valeur de AsyncPaginate}
-  // onChange = {qui est le moment ou la valeur change}
-    // et donc la mettre à jour avec un {handleOnchange} qui sera sa valeur.
+// un placeholder = "la valeur du place holder"
+// un debounceTimout = {delai entre chaque requete}
+// value = {la valeur de AsyncPaginate}
+// onChange = {qui est le moment ou la valeur change}
+// et donc la mettre à jour avec un {handleOnchange} qui sera sa valeur.
 // On crée la const [search, setSearch] = useState (initialState = etat initial de l'app)
